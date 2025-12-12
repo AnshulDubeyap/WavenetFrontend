77d7cb54d66966011bfeaf1a945963104187bdf4
Anshul Dubey <dubeyanshul2204@gmail.com>
2025-12-13 00:00:00 +0530

    final

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://wavenet-backend-9wvk.onrender.com/api",
+  baseURL: "https://wavenetbackend.onrender.com",
   withCredentials: true,
 });
 
